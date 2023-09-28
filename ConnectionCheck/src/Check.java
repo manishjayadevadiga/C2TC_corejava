d72a170972e58a03c34219271e9607aacd0a67c3
dell <dell@DESKTOP-EHT3QTL>
2023-09-28 22:17:26 +0530

    first commit

---
+++ ConnectionCheck/src/Check.java
@@ -0,0 +1,11 @@
+
+public class Check {
+
+	public static void main(String[] args) {
+		//program to check whether eclipse is connected to github repository
+
+		
+		System.out.println(" Connection Successful");
+	}
+
+}
