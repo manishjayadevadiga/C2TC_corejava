99c5345f42aa64a2e585abd23ef65cee55df979b
dell <dell@DESKTOP-EHT3QTL>
2023-09-29 08:45:32 +0530

    first commit

---
+++ FinalCheck/src/finalchecking.java
@@ -0,0 +1,9 @@
+
+public class finalchecking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Checking final push of future files after error occured as non fast forward to master");
+	}
+
+}
