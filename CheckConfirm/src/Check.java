624db97a3a75ac7956f952c163d015c0cd32dcf8
dell <dell@DESKTOP-EHT3QTL>
2023-09-29 07:59:05 +0530

    first commit

---
+++ CheckConfirm/src/Check.java
@@ -0,0 +1,13 @@
+
+public class Check {
+
+	public static void main(String[] args) {
+		
+		//checking whether I must use GitHub password /token in password
+		//..section of last Login dialog box.
+		
+		System.out.println("Checked");
+
+	}
+
+}
